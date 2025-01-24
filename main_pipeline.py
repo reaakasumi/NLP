48b5ce8db0e031d5024d61d71f82723165a4cf1b
Theresa <92042502+Resi06@users.noreply.github.com>
2025-01-24 12:43:11 +0100

    Update main_pipeline.py

--- main_pipeline.py
+++ main_pipeline.py
@@ -4,14 +4,15 @@ import subprocess
 scripts = [
     "main_analysis.py",                    # Step 1: Main analysis
     # "preprocessing_to_conllu.py",          # Step 2: Preprocessing
-    # "naive_bayes.py",                      # Step 3: Naive Bayes model
-    # "BERTScore_fine_tuned.py",             # Step 4: BERTScore fine-tuning
-    # "BERTScore_semantic_similarity.py",    # Step 5: BERTScore semantic similarity
-    # "create_features.py"                 # Step 6: Create Features for feature classifier
-    # "feature_classifier.py"              # Step 7: Feature Classifier
-    "naive_bayes_analysis.py",             # Step 8: Naive Bayes analysis
+    "naive_bayes.py",                        # Step 3: Naive Bayes model
+    "naive_bayes_analysis.py",                #further Analysis on Naive Bayes
+    "vectara_improved.py",                    #Step 4: Vectara
+    # "BERTScore_fine_tuned.py",             # Step 5: BERTScore fine-tuning
+    # "BERTScore_semantic_similarity.py",    # Step 6: BERTScore semantic similarity
+    # "create_features.py"                 # Step 7: Create Features for feature classifier
+    # "feature_classifier.py"              # Step 8: Feature Classifier
     "bertscore_fine_tuned_analysis.py",    # Step 9: Analysis for BERT fine-tuned model
-    "bertscore_semantic_similarity_analysis.py"  # Step 8: Analysis for semantic similarity
+    "bertscore_semantic_similarity_analysis.py"  # Step 10: Analysis for semantic similarity
 ]
 
 # Execute each script sequentially
